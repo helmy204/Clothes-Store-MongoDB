c889bfe94fbf4df070aecc6b46b362e2813a175d
a.asalama@dev.thiqah.sa <a.asalama@dev.thiqah.sa>
2020-04-22 09:50:03 +0300

    Remove Category

--- ClothesStoreMongoDB - Before/src/ClothesStore/ClothesStore/Models/Category.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ClothesStore.Models
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- ClothesStoreMongoDB - Before/src/ClothesStore/ClothesStore/Models/Product.cs
+++ ClothesStoreMongoDB - Before/src/ClothesStore/ClothesStore/Models/Product.cs
@@ -4,8 +4,5 @@
     {
         public string Id { get; set; }
         public string Name { get; set; }
-
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
     }
 }
